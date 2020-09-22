8b9e4ec96672c054b1392d94de79b35f47fec6cc
Matt Durr <mdurr@dotdash.com>
2020-09-22 14:56:28 -0400

    Add lift for inside loggert...

--- src/Logging/Log4hs.hs
+++ src/Logging/Log4hs.hs
@@ -1,75 +1,80 @@
-module Logging.Log4hs (defLoggerConfig,defLoggerCtx, mkLogState, logMsg, logShow, logIO, module Logging.Log4hs.Types) where
-import           Control.Applicative             ((<|>))
-import           Control.Concurrent              (myThreadId)
-import           Control.Monad.IO.Class          (MonadIO)
-import           Control.Monad.Reader
-import           Data.Foldable                   (asum)
-import           Data.Maybe                      (mapMaybe)
-import           Data.Text                       (Text, pack)
-import           Data.Time.Clock                 (getCurrentTime)
-import           Logging.Log4hs.Appender.Console
-import           Logging.Log4hs.Layout.Pattern
-import           Logging.Log4hs.Types
-import           System.Process                  (getProcessId)
+module Logging.Log4hs (defLoggerConfig, defLoggerCtx, mkLogState, logMsg, logShow, logIO, logLift, module Logging.Log4hs.Types) where
+
+import Control.Applicative ((<|>))
+import Control.Concurrent (myThreadId)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader
+import Data.Foldable (asum)
+import Data.Maybe (mapMaybe)
+import Data.Text (Text, pack)
+import Data.Time.Clock (getCurrentTime)
+import Logging.Log4hs.Appender.Console
+import Logging.Log4hs.Layout.Pattern
+import Logging.Log4hs.Types
+import System.Process (getProcessId)
 
 defLoggerConfig :: LogConfig
-defLoggerConfig = LogConfig { configLevel = WARN
-                            , configAppenders = ["console"]
-                            , configChildren = []
-                            , configName = ""}
+defLoggerConfig =
+  LogConfig
+    { configLevel = WARN,
+      configAppenders = ["console"],
+      configChildren = [],
+      configName = ""
+    }
 
 defLoggerCtx :: (MonadIO m, LogVarProvider m) => LogContext m
-defLoggerCtx = LogContext { ctxAppenders = [("console",consoleAppender SYSTEM_OUT (patternLayout (pack "%maxLen{%d{%T%Q}}{13} [%T] %level %logger{36} - %msg%n")))]
-                          , ctxConfigs = defLoggerConfig
-                          , ctxTime = liftIO getCurrentTime
-                          , ctxProcessId = liftIO getProcessId
-                          , ctxThreadId = liftIO myThreadId
-                          }
-
+defLoggerCtx =
+  LogContext
+    { ctxAppenders = [("console", consoleAppender SYSTEM_OUT (patternLayout (pack "%maxLen{%d{%T%Q}}{13} [%T] %level %logger{36} - %msg%n")))],
+      ctxConfigs = defLoggerConfig,
+      ctxTime = liftIO getCurrentTime,
+      ctxProcessId = liftIO getProcessId,
+      ctxThreadId = liftIO myThreadId
+    }
 
 mkLogState :: Monad n => LogContext n -> n (LogState n)
 mkLogState ctx = do
-  apps <- mapM (\(n,am) -> am >>= \a -> return (n,a)) $ ctxAppenders ctx
-  return LogState{context=ctx,appenders=apps}
+  apps <- mapM (\(n, am) -> am >>= \a -> return (n, a)) $ ctxAppenders ctx
+  return LogState {context = ctx, appenders = apps}
 
-
-getAppenders :: [(String,(LogAppender n, LogAppenderFinish n))] -> LogConfig -> [LogAppender n]
-getAppenders apps cfg = mapMaybe (`lookup` map (\(n,(a,_)) -> (n,a)) apps) (configAppenders cfg)
+getAppenders :: [(String, (LogAppender n, LogAppenderFinish n))] -> LogConfig -> [LogAppender n]
+getAppenders apps cfg = mapMaybe (`lookup` map (\(n, (a, _)) -> (n, a)) apps) (configAppenders cfg)
 
 getConfig :: (Monad m, HasLog m l) => [String] -> m LogConfig
 getConfig [] = ctxConfigs . context <$> logState
 getConfig nm = do
-    ctx <- context <$> logState
-    case asum (map (findConfig nm) (configChildren $ ctxConfigs ctx)) of
-        Nothing -> return $ ctxConfigs ctx
-        Just c  -> return c
-    where findConfig [] _ = Nothing
-          findConfig [nm'] cfg
-            | configName cfg == nm' = Just cfg
-            | otherwise =  Nothing
-          findConfig (nm':nms) cfg
-            | configName cfg == nm' = asum (map (findConfig nms) (configChildren cfg)) <|> Just cfg
-            | otherwise = Nothing
+  ctx <- context <$> logState
+  case asum (map (findConfig nm) (configChildren $ ctxConfigs ctx)) of
+    Nothing -> return $ ctxConfigs ctx
+    Just c -> return c
+  where
+    findConfig [] _ = Nothing
+    findConfig [nm'] cfg
+      | configName cfg == nm' = Just cfg
+      | otherwise = Nothing
+    findConfig (nm' : nms) cfg
+      | configName cfg == nm' = asum (map (findConfig nms) (configChildren cfg)) <|> Just cfg
+      | otherwise = Nothing
 
-
-logMsg :: (HasLog m l) => [String] -> LogLevel -> Text -> [(Text,Text)] -> m ()
+logMsg :: (HasLog m l) => [String] -> LogLevel -> Text -> [(Text, Text)] -> m ()
 logMsg nm lvl msg args = do
-    c <- getConfig nm
-    a <- flip getAppenders c . appenders <$> logState
-    case (configLevel c, lvl) of
-        (ERROR,ERROR) -> dolog a
-        (WARN,ERROR)  -> dolog a
-        (WARN,WARN)   -> dolog a
-        (INFO,ERROR)  -> dolog a
-        (INFO,WARN)   -> dolog a
-        (INFO,INFO)   -> dolog a
-        (DEBUG,ERROR) -> dolog a
-        (DEBUG,WARN)  -> dolog a
-        (DEBUG,INFO)  -> dolog a
-        (DEBUG,DEBUG) -> dolog a
-        (TRACE,_)     -> dolog a
-        (_,_)         -> return ()
-    where dolog app = subLog $ mapM_ (\a -> a nm lvl msg args) app
+  c <- getConfig nm
+  a <- flip getAppenders c . appenders <$> logState
+  case (configLevel c, lvl) of
+    (ERROR, ERROR) -> dolog a
+    (WARN, ERROR) -> dolog a
+    (WARN, WARN) -> dolog a
+    (INFO, ERROR) -> dolog a
+    (INFO, WARN) -> dolog a
+    (INFO, INFO) -> dolog a
+    (DEBUG, ERROR) -> dolog a
+    (DEBUG, WARN) -> dolog a
+    (DEBUG, INFO) -> dolog a
+    (DEBUG, DEBUG) -> dolog a
+    (TRACE, _) -> dolog a
+    (_, _) -> return ()
+  where
+    dolog app = subLog $ mapM_ (\a -> a nm lvl msg args) app
 
 logShow :: (HasLog m l, Show s) => [String] -> LogLevel -> s -> m ()
 logShow nm l s = logMsg nm l (pack (show s)) []
@@ -77,3 +82,8 @@ logShow nm l s = logMsg nm l (pack (show s)) []
 logIO :: LogState IO -> LoggerT IO IO () -> IO ()
 logIO st act = runReaderT (runLoggerT act) st
 
+logLift :: Monad m => m a -> LoggerT m n a
+logLift n =
+  LoggerT
+    { runLoggerT = lift n
+    }
--- src/Logging/Log4hs/Types.hs
+++ src/Logging/Log4hs/Types.hs
@@ -1,88 +1,84 @@
-{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
-
-{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Logging.Log4hs.Types where
-import           Control.Concurrent        (ThreadId, myThreadId)
-import           Control.Monad.IO.Class    (MonadIO, liftIO)
-import           Control.Monad.Reader
-import Control.Monad.Fail (MonadFail(..))
+
+import Control.Concurrent (ThreadId, myThreadId)
+import Control.Monad.Fail (MonadFail (..))
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader
 import Control.Monad.Reader.Class (ask)
-import           Control.Monad.Trans.Class (lift)
-import           Data.Text                 (Text)
-import           Data.Time.Clock           (UTCTime, getCurrentTime)
-import           System.Process            (getProcessId)
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import System.Process (getProcessId)
 
+data LogLevel = ERROR | WARN | INFO | DEBUG | TRACE deriving (Show, Eq, Read)
 
-data LogLevel = ERROR | WARN | INFO | DEBUG | TRACE deriving (Show,Eq,Read)
+type Layout m = [String] -> LogLevel -> Text -> [(Text, Text)] -> m Text
 
-type Layout m = [String] -> LogLevel -> Text -> [(Text,Text)] -> m Text
-
-type LogAppender m = [String] -> LogLevel -> Text -> [(Text,Text)] -> m ()
+type LogAppender m = [String] -> LogLevel -> Text -> [(Text, Text)] -> m ()
 
 type LogAppenderFinish m = m ()
 
+data LogContext m = LogContext
+  { ctxAppenders :: [(String, m (LogAppender m, LogAppenderFinish m))],
+    ctxConfigs :: LogConfig,
+    ctxTime :: m UTCTime,
+    ctxProcessId :: m Int,
+    ctxThreadId :: m ThreadId
+  }
 
-data LogContext m = LogContext { ctxAppenders :: [(String, m (LogAppender m,LogAppenderFinish m))]
-                               , ctxConfigs   :: LogConfig
-                               , ctxTime      :: m UTCTime
-                               , ctxProcessId :: m Int
-                               , ctxThreadId  :: m ThreadId
-                               }
-
-data LogState m = LogState {context :: LogContext m, appenders :: [(String,(LogAppender m, LogAppenderFinish m))]}
+data LogState m = LogState {context :: LogContext m, appenders :: [(String, (LogAppender m, LogAppenderFinish m))]}
 
 --data ReliabilityStrategy = ReliabilityStrategy {}
-data LogConfig = LogConfig { configLevel     :: LogLevel
-                           , configAppenders :: [String]
-                           --, configReliabilityStrategy :: ReliabilityStrategy
-                           , configChildren  :: [LogConfig]
-                           , configName      :: String
-                           }
+data LogConfig = LogConfig
+  { configLevel :: LogLevel,
+    configAppenders :: [String],
+    --, configReliabilityStrategy :: ReliabilityStrategy
+    configChildren :: [LogConfig],
+    configName :: String
+  }
 
 -- logger state with underlying monad m where logging will actually happen ex: IO
-newtype LoggerT n m a = LoggerT { runLoggerT :: ReaderT (LogState m) n a }
+newtype LoggerT n m a = LoggerT {runLoggerT :: ReaderT (LogState m) n a}
 
 instance (Monad n, Monad m) => Functor (LoggerT n m) where
-    fmap ab fa = LoggerT { runLoggerT = ab <$> runLoggerT fa}
+  fmap ab fa = LoggerT {runLoggerT = ab <$> runLoggerT fa}
 
 instance (Monad n, Monad m) => Applicative (LoggerT n m) where
-    pure a = LoggerT { runLoggerT = pure a}
-    (<*>) fab fa = LoggerT { runLoggerT = runLoggerT fa >>= \a -> runLoggerT fab >>= \ab -> return $ ab a}
+  pure a = LoggerT {runLoggerT = pure a}
+  (<*>) fab fa = LoggerT {runLoggerT = runLoggerT fa >>= \a -> runLoggerT fab >>= \ab -> return $ ab a}
 
 instance (Monad n, Monad m) => Monad (LoggerT n m) where
-    return = pure
-    (>>=) ma amb = LoggerT { runLoggerT = runLoggerT ma >>= \a -> runLoggerT (amb a)}
+  return = pure
+  (>>=) ma amb = LoggerT {runLoggerT = runLoggerT ma >>= \a -> runLoggerT (amb a)}
 
 instance (MonadIO n, Monad m) => MonadIO (LoggerT n m) where
-    liftIO ioa = LoggerT { runLoggerT = liftIO $ ioa}
+  liftIO ioa = LoggerT {runLoggerT = liftIO $ ioa}
 
 class Monad m => LogVarProvider m where
-    provideTime :: m UTCTime
-    provideProcessId :: m Int
-    provideThreadId :: m ThreadId
+  provideTime :: m UTCTime
+  provideProcessId :: m Int
+  provideThreadId :: m ThreadId
 
 instance (MonadFail m, MonadFail n) => MonadFail (LoggerT m n) where
-    fail m = LoggerT { runLoggerT = Control.Monad.Reader.fail m}
+  fail m = LoggerT {runLoggerT = Control.Monad.Reader.fail m}
 
 instance LogVarProvider IO where
-    provideTime = getCurrentTime
-    provideProcessId = getProcessId
-    provideThreadId = myThreadId
+  provideTime = getCurrentTime
+  provideProcessId = getProcessId
+  provideThreadId = myThreadId
 
 instance LogVarProvider (LoggerT IO IO) where
-    provideTime = LoggerT { runLoggerT = liftIO getCurrentTime }
-    provideProcessId = LoggerT { runLoggerT = liftIO getProcessId}
-    provideThreadId = LoggerT { runLoggerT = liftIO myThreadId}
-
+  provideTime = LoggerT {runLoggerT = liftIO getCurrentTime}
+  provideProcessId = LoggerT {runLoggerT = liftIO getProcessId}
+  provideThreadId = LoggerT {runLoggerT = liftIO myThreadId}
 
 class (Monad m, Monad n) => HasLog m n | m -> n where
-    logState :: m (LogState n)
-    subLog :: n a -> m a
+  logState :: m (LogState n)
+  subLog :: n a -> m a
 
 instance HasLog (LoggerT IO IO) IO where
-    logState = LoggerT ask
-    subLog s = LoggerT { runLoggerT = lift s}
-
-
+  logState = LoggerT ask
+  subLog s = LoggerT {runLoggerT = lift s}
